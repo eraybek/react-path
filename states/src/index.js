231ac2091b48f2079bd469f97a7c4a72f6e1794d
Eray Aybek <aybek17@itu.edu.tr>
2023-02-03 17:44:13 +0300

    added state definition for input

--- states/src/index.js
+++ states/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from "./components/Counter"
+// import Counter from "./components/Counter"
+import InputExample from "./components/InputExample"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
     {/* <App /> */}
-    <Counter />
+    {/* <Counter /> */}
+    <InputExample/>
     
   </React.StrictMode>
 );
